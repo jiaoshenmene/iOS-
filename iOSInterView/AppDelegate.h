0a38807419ff86af2d45882b482f615289a8ae66
杜甲 <dujia211294@sogou-inc.com>
2019-03-15 14:14:00 +0800

    first

---
+++ iOSInterView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSInterView
+//
+//  Created by 杜甲 on 2019/3/15.
+//  Copyright © 2019 杜甲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSInterView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSInterView
+//
+//  Created by 杜甲 on 2019/3/15.
+//  Copyright © 2019 杜甲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
